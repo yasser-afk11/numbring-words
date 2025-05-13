bad4b1dd4c9161c77fcb2c7e7733921166a9a343
yasser-afk11 <143416370+yasser-afk11@users.noreply.github.com>
2025-05-13 01:34:47 +0100

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,18 +2,22 @@
 using namespace  std;
 
 int main(){
-//here we are going to take the data we need 
+//here we are collecting data from the user about lenght/count...
 string c;
+int count;
 int lenght;
 cout<<"************************hello**************************"<<endl;
 cout<<"--in this code im going to help you number your words--"<<endl;
 cout<<"please tell me how much words you have: ";
 cin>>lenght;
+cout<<"from where you want to start counting?: ";
+cin>>count;
 string array[lenght];
+//i did the following two ligne like that to clarify what im doing , you can delet it and delet the '=' in the first for as you see
 int index;
 index = lenght-1;
 
-//this for let us import the words
+//this for to collect the words
 for (int j=0 ; j <= index; j++)
 {
 	int a;
@@ -23,11 +27,12 @@ for (int j=0 ; j <= index; j++)
 	array[j] = c;	
 }
 
-//this for let us output the words with their number at left
+
+//this for is to output the words numbred
 for (int i =0; i<=index; i++ )
     {
     	int b;
-    	b = i+1;
+    	b = count+i;
         cout<<b;
 		cout<<". ";
 		cout<<array[i]<<endl;
@@ -35,6 +40,5 @@ for (int i =0; i<=index; i++ )
 
     return 0;
 }
-
 
 
