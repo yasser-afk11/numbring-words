8cf87b52857f9decee3a5c9cc268f309f622bc4e
yasser-afk11 <143416370+yasser-afk11@users.noreply.github.com>
2025-05-13 01:18:30 +0100

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace  std;
+
+int main(){
+string c;
+int lenght;
+cout<<"************************hello**************************"<<endl;
+cout<<"--in this code im going to help you number your words--"<<endl;
+cout<<"please tell me how much words you have: ";
+cin>>lenght;
+string array[lenght];
+
+int index;
+index = lenght-1;
+
+for (int j=0 ; j <= index; j++)
+{
+	int a;
+	a = j+1;
+	cout<<"type your "<<a<<" word: ";
+	cin>>c;
+	array[j] = c;	
+}
+
+
+
+
+for (int i =0; i<=index; i++ )
+    {
+    	int b;
+    	b = i+1;
+        cout<<b;
+		cout<<". ";
+		cout<<array[i]<<endl;
+    }
+
+    return 0;
+}
+
+
+
